83988adcb91e15ce1d28f90b53dea3fe12e53316
kranz nikolai fontanilla <znark912@outlook.com>
2017-05-07 17:59:59 -0400

    Updated

--- Problem 4 LargestPalindromeProduct.py
+++ Problem 4 LargestPalindromeProduct.py
@@ -1,19 +1,22 @@
 def CheckIfNumberIsPalindrome(x):
-    Reverse=0
-    Number=x
-    while(Number>0):
-        Reminder = Number%10
-        Reverse = (Reverse *10) + Reminder
-        Number= Number//10
-    return x==Reverse
+    ispalindrome=True
+    i=0
+    x=str(x)
+    while((i<len(x)/2) and ispalindrome):
+        if(x[i]!=x[-(i+1)]):
+            ispalindrome= False
+        i+=1
+    return ispalindrome
 def getLargestPalindrome(min,max):
     largest=0
-    for x in range(min,max):
-        for y in range(min,max):
+    for x in range(max,min,-1):
+        for y in range(x,min,-1):
             product= y*x
-            isPalindrome = CheckIfNumberIsPalindrome(product)
-            if(isPalindrome):
-                if(product>largest):
+            if (product > largest):
+                isPalindrome = CheckIfNumberIsPalindrome(product)
+                if(isPalindrome):
                     largest= product
+            else:
+                break
     return largest
 print(getLargestPalindrome(100,1000))
